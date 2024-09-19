1b10cc15d91b2448e2d382e3968db9f08a8271b4
Niko Strijbol <strijbol.niko@gmail.com>
2024-09-19 17:14:04 +0200

    Fix another bug

--- lib/asciidoctor/doctest/test_reporter.rb
+++ lib/asciidoctor/doctest/test_reporter.rb
@@ -70,7 +70,7 @@ module Asciidoctor
           ("#{skips} skipped".color(:cyan) if skips > 0)
         ].compact.join(', ') + ")\n\n")
 
-        str << "Finished in %.3f s.\n".format(total_time)
+        str << format("Finished in %.3f s.\n", total_time)
 
         str << "\nYou have skipped tests. Run with VERBOSE=yes for details.\n" if results.any?(&:skipped?) && !verbose?
 
